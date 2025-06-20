245200b5faeb994ce8803872f37f8453e12cf676
Harshad Dhongade <harshaddhongade0901@gmail.com>
2025-06-20 20:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//Dark-White Mode
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("white-mode");
+}
+
+// Particles Background Animation
+particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('particles.js loaded');
+});
